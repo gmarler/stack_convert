ee034d14d33cb765d23e0dbde9ae21e41cbf19b9
gmarler <gmarler@gmarler.com>
2017-12-27 17:40:40 -0500

    Clean up whitespace.

--- stack_convert/node.py
+++ stack_convert/node.py
@@ -1,26 +1,23 @@
-
 class Node:
   def __init__(self, name, value=0, children={}):
-    self.name  = name
+    self.name = name
     self.value = value
     self.children = children
 
-
   def add(self, frames, value):
-   self.value += value
+    self.value += value
 
-   if (frames and len(frames) > 0):
-     head = frames[0]
-     child = self.children[head] if self.children[head] else None
-     if (child is None):
-       child = Node(head)
-       self.children[head] = child
-     frames[0:1] = []
-     child.add(frames, value)
-
+    if (frames and len(frames) > 0):
+      head = frames[0]
+      child = self.children[head] if self.children[head] else None
+      if (child is None):
+        child = Node(head)
+        self.children[head] = child
+      frames[0:1] = []
+      child.add(frames, value)
 
   def serialize(self):
-    res = { 'name': self.name, 'value': self.value }
+    res = {'name': self.name, 'value': self.value}
 
     children = []
 
@@ -31,5 +28,3 @@ class Node:
       res.children = children
 
     return res
-
-
