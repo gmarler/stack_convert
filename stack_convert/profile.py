84ffbc37f59daabd6aca8fac7910e717feefc0ad
gmarler <gmarler@gmarler.com>
2017-12-27 17:36:06 -0500

    Completed Profile methods

--- stack_convert/profile.py
+++ stack_convert/profile.py
@@ -1,4 +1,5 @@
-import Node
+from .node import Node
+
 
 class Profile:
   def __init__(self, samples=Node('root'), stack=[]):
@@ -7,7 +8,21 @@ class Profile:
     self.stack_is_open = 0
     self.name = None
 
-  def openStack(self,name):
+  def openStack(self, name):
     self.stack_is_open = 1
     self.name = name
+
+  def addFrame(self, func, mod):
+    # TODO: skip process name and some punctuation
+    # prepend function to list
+    self.stack.insert(0, func)
+
+  def closeStack(self, value):
+    self.stack.insert(0, self.name)
+    self.samples.add(self.stack, value)
+
+    self.stack([])
+    self.name(None)
+    self.stack_is_open(0)
+
 
