c69c3efac60d81fcb0e9f70b9f3bbb63abad41e2
gmarler <gmarler@gmarler.com>
2017-12-29 10:11:52 -0500

    replace use of re.match with re.search

--- stack_convert/parser.py
+++ stack_convert/parser.py
@@ -1,6 +1,16 @@
 from .profile import Profile
+import re
 
 class Parser:
   def raw(self,filename):
+    fh = open(filename,"r")
+    for line in fh:
+      line.rstrip('\n')
 
-
+      if re.search(r"^\s*$", line):
+        continue
+      if re.search(r"^$", line):
+        continue
+      m = re.search(r"^\s*(\d+)$", line)
+      if m:
+        count = m[0]
