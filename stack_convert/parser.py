62cd1abe944e69da88433105ea13703ed1aa958b
gmarler <gmarler@gmarler.com>
2017-12-29 12:23:13 -0500

    More Node tests

--- stack_convert/parser.py
+++ stack_convert/parser.py
@@ -2,8 +2,14 @@ from .profile import Profile
 import re
 
 class Parser:
-  def raw(self,filename):
-    fh = open(filename,"r")
+  def __init__(self):
+    self.profile = None
+
+  def parseDTrace(self, filename):
+    fh = open(filename, "r")
+
+    self.profile = Profile()
+
     for line in fh:
       line.rstrip('\n')
 
@@ -11,6 +17,48 @@ class Parser:
         continue
       if re.search(r"^$", line):
         continue
+
       m = re.search(r"^\s*(\d+)$", line)
       if m:
         count = m[0]
+        if (profile.stack_is_open):
+          self.profile.closeStack(count)
+        else:
+          print("ERROR: %s", line)
+        continue
+
+      frame = line
+
+      # Strip leading whitespace
+      frame = re.sub(r'^\s*', '', frame)
+      # Strip offset into function
+      frame = re.sub(r'\+[^+]*$', '', frame)
+      # Remove args from C++ function names
+      frame = re.sub(r'(::.*)[(<].*', r'\1', frame)
+      # Denote separator between kernel and user stacks with '-'
+      if re.search(r"^$", frame):
+        frame = "-"
+
+      if not re.search(r"^(\S+)`(\S+)$", frame):
+        print("UNKNOWN LINE: %s", frame)
+        contine
+
+      if (profile.stack_is_open()):
+        print("Sending to profile addFrame with: (%s, None)", frame)
+        profile.addFrame(frame, None)
+      else:
+        profile.openStack('kernel')
+        print("Sending to profile addFrame with: (%s, None)", frame)
+        profile.addFrame(frame, None)
+
+    return self.profile
+
+  def serializeProfile(self):
+    """Serialize Profile Samples"""
+    return self.profile.samples.serialize()
+
+  def encodeAsJSON(self, serializedProfile=None):
+    """Encode a Serialized Profile as JSON"""
+    import json
+    encoded = json.dumps(serializedProfile)
+    return encoded
--- tests/test_node.py
+++ tests/test_node.py
@@ -9,3 +9,17 @@ def test_defaults():
   assert n1.name == n2.name
   assert n1.value == n2.value
   assert n1.children == n2.children
+
+def test_single_node():
+  n1 = Node("lwp_start", 5)
+  assert n1.name == "lwp_start"
+  assert n1.value == 5
+
+def test_double_node():
+  n1 = Node("lwp_start", 5)
+  n2 = Node("lwp_park", 5, {'lwp_start': n1})
+  assert len(n2.children) == 1
+  assert n1.value == 5
+  assert n2.value == 5
+  assert n2.children['lwp_start'] == n1
+  assert n2.children['lwp_start'].value == 5
