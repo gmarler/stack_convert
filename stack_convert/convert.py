5177b9687dbeac006685fa660d0024c2a848d38a
Gordon Marler <gmarler@gmarler.com>
2018-01-05 20:31:51 -0500

    Add stubs for LoggingConfig/ApplicationConfig

--- stack_convert/convert.py
+++ stack_convert/convert.py
@@ -8,7 +8,7 @@ class Convert:
   def __init__(self):
     argparser = argparse.ArgumentParser(description="Convert stacks into JSON")
     argparser.add_argument("-i", "--inputFile", action="store", type=str,
-                        help="raw stack data to parse")
+                        help="file containing raw stack data to parse")
     argparser.add_argument("-t", "--inputType", action="store", type=str, default="",
                         help="type of input data (selects correct parser)")
     self.config = argparser.parse_args()
@@ -19,3 +19,23 @@ class Convert:
     self.stackparser.serializeProfile()
     json_data = self.stackparser.encodeAsJSON()
     print(json_data)
+
+
+class LoggingConfig:
+  def __enter__(self, filename="log_config.yaml"):
+    # logging.config.dictConfig( yaml.load(filename))
+    pass
+  def __exit__(self, *exc):
+    pass
+    # logging.shutdown()
+
+class ApplicationConfig:
+  def __enter__(self):
+    # Build os.environ defaults.
+    # Load files.
+    # Build ChainMap from environs and files.
+    # Parse command-line arguments.
+    return namespace
+
+  def __exit__(self, *exc):
+    pass
