c1232f2be435dced23fbdacd35c170e4a7af6a38
Gordon Marler <gmarler@gmarler.com>
2018-09-07 10:29:56 -0400

    script now deployable - updated to latest d3-flame-graph

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
   name='stack_convert',
-  version='0.0.1',
+  version='0.0.2',
   license='MIT',
   description='Stack Conversion to D3 FlameGraph JSON Format',
 
   author='Gordon Marler',
   author_email='gmarler@gmarler.com',
 
-  packages=find_packages(where='stack_convert'),
-  package_dir={'': 'stack_convert'},
+  packages=['stack_convert'],
 
   install_requires=['pytest', 'pytest-mock', 'pytest-datafiles', 'pyaml'],
   tests_require=['pytest', 'pytest-mock', 'pytest-datafiles', 'pyaml'],
